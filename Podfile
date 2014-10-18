74f026d1ad1c44bd5fc052335b889519e26bb56d
Thibaut LE LEVIER <thibaut@lelevier.fr>
2014-10-18 21:50:00 +0200

    basic sound upload to test

--- Podfile
+++ Podfile
@@ -2,3 +2,5 @@ platform :ios, '7.0'
 inhibit_all_warnings!
 
 pod 'AFNetworking', '~> 2.4'
+pod 'MBProgressHUD', '~> 0.9'
+pod 'MBProgressHUDExtensions', '~> 0.0'
--- bullshitmetter/ViewController.m
+++ bullshitmetter/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 #import <AVFoundation/AVFoundation.h>
+#import "UIViewController+MBProgressHUD.h"
+#import <AFNetworking/AFNetworking.h>
 
 @interface ViewController () //<AVAudioRecorderDelegate>
 
@@ -97,6 +99,33 @@
     
     AVAudioSession *audioSession = [AVAudioSession sharedInstance];
     [audioSession setActive:NO error:nil];
+    
+    [self showHUD];
+    
+    NSData *file = [NSData dataWithContentsOfURL:self.fileURL];
+    
+    AFHTTPRequestOperationManager *manager = [[AFHTTPRequestOperationManager alloc] initWithBaseURL:[NSURL URLWithString:@"http://92.222.1.55"]];
+    
+    AFHTTPRequestOperation *op = [manager POST:@"/sound"
+                                    parameters:nil
+                     constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
+
+                         [formData appendPartWithFileData:file
+                                                     name:@"Bullshit"
+                                                 fileName:@"Bullshit.m4a"
+                                                 mimeType:@"audio/m4a"];
+                         
+                                        }
+                                       success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                                           [self hideHUD];
+
+                                           
+                                        }
+                                       failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                                           [self hideHUDWithCompletionMessage:error.localizedDescription];
+                                        }];
+    
+    [op start];
 }
 
 @end
