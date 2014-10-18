d812f0b4464f7a69cdad2d13da2151d8c87b22ea
Thibaut LE LEVIER <thibaut@lelevier.fr>
2014-10-18 14:56:17 +0200

    Initial Commit

---
+++ bullshitmetter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bullshitmetter
+//
+//  Created by Thibaut LE LEVIER on 18/10/2014.
+//  Copyright (c) 2014 Thibaut LE LEVIER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bullshitmetter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bullshitmetter
+//
+//  Created by Thibaut LE LEVIER on 18/10/2014.
+//  Copyright (c) 2014 Thibaut LE LEVIER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
